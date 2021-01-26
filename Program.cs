d98badea634d772aa04c692e12d00da2ac972f74
Andrew Moore <mooreandrew015@gmail.com>
2021-01-26 18:48:29 -0500

    with extra notes for 10 jokes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        class Joke
+        {
+            public int id { get; set; }
+            public string type { get; set; }
+            public string setup { get; set; }
+            public string punchline { get; set; }
+
+
+
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync($"https://official-joke-api.appspot.com/random_joke");
+
+
+            // var jokes = await JsonSerializer.DeserializeAsync<List<Joke>>(responseAsStream);
+            var jokes = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
+
+
+
+            // foreach (var Joke in jokes)
+            // {
+
+            Console.WriteLine($"The type {jokes.type} setup {jokes.setup} and punchline {jokes.punchline}");
+            // }
+
+
+            // Console.WriteLine("Would you like another joke?");
+
+
+
+
+        }
     }
-  }
 }
